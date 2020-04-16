ebaa38ad548a0db752bc5961160b512b37690118
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-04-16 11:01:50 -0300

    Upload de multiplas imagens

--- classes/Painel.php
+++ classes/Painel.php
@@ -83,7 +83,7 @@
 				$imagem['type'] == 'image/png'){
  
 				$tamanho = intval($imagem['size']/1024);
-				if($tamanho < 300)
+				if($tamanho < 900)
 					return true;
 				else
 					return false;
--- painel/pages/cadastrar-produtos.php
+++ painel/pages/cadastrar-produtos.php
@@ -1,7 +1,58 @@
 <div class="box-content">
     <h2><i class="fa fa-pencil"></i> Cadastrar Produto</h2>
-
+    
     <form method="post" enctype="multipart/form-data">
+    <?php
+        if (isset($_POST['acao'])) {
+            $nome = $_POST['nome'];
+            $descicao = $_POST['descricao'];
+            $largura = $_POST['largura'];
+            $altura = $_POST['altura'];
+            $peso = $_POST['peso'];
+            $comprimento = $_POST['comprimento'];
+            $quantidade = $_POST['quantidade'];
+
+            $imagens = array();
+            $amountFiles = count($_FILES['imagem']['name']);
+
+            $sucesso = true;
+
+            if ($_FILES['imagem']['name'][0] != '') {             
+
+                for ($i=0; $i < $amountFiles; $i++) { 
+                    $imagemAtual = ['type'=>$_FILES['imagem']['type'][$i],
+                    'size'=>$_FILES['imagem']['size'][$i]];
+                    if (Painel::imagemValida($imagemAtual) == false) {
+                        $sucesso = false;
+                        Painel::alert('erro', 'Uma das imagens selecionadas é inválida! ');
+                        break;
+                    }
+                }
+
+                if ($sucesso) {
+                    //Cadastrar informações, imagens e realizar upload
+                    for ($i=0; $i < $amountFiles; $i++) { 
+                        $imagemAtual = ['tmp_name'=>$_FILES['imagem']['tmp_name'][$i],
+                            'name'=>$_FILES['imagem']['name'][$i]];
+                        $imagens[] = Painel::uploadFile($imagemAtual);
+                    }
+
+                    $sql = MySql::conectar()->prepare("INSERT INTO `tb_admin.estoque` VALUES (null,?,?,?,?,?,?,?)");
+                    $sql->execute(array($nome,$descicao,$largura,$altura,$comprimento,$peso,$quantidade));
+                    $lastId = MySql::conectar()->lastInsertId();
+                    foreach ($imagens as $key => $value) {
+                        MySql::conectar()->exec("INSERT INTO `tb_admin.estoque_imagens` VALUES (null,$lastId,'$value')");
+                    }
+                    Painel::alert('sucesso', 'O produto foi cadastrado com sucesso!');
+                }
+
+        }else {
+            $sucesso = false;
+            Painel::alert('erro','Você precisa selecionar pelo menos uma imagem');
+        }
+            
+        }
+    ?>
         <div class="form-group">
             <label>Nome do Produto:</label>
 			<input type="text" name="nome">
@@ -26,10 +77,20 @@
             <label>Comprimento do Produto:</label>
 			<input type="number" name="comprimento" min="0" max="900" value="0">
         </div><!--form-group-->
+
+        <div class="form-group">
+            <label>Peso do Produto:</label>
+			<input type="number" name="peso" min="0" max="900" value="0">
+        </div><!--form-group-->
+
+        <div class="form-group">
+            <label>Quantidade Atual do Produto:</label>
+			<input type="number" name="quantidade" min="0" max="900" value="0">
+        </div><!--form-group-->
         
         <div class="form-group">
             <label>Selecione a Imagens:</label>
-			<input multiple type="file" name="imagem">
+			<input multiple type="file" name="imagem[]">
         </div><!--form-group-->
         <input type="submit" name="acao" value="Cadastrar Produto!">
     </form>
