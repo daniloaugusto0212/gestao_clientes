9a1fe4a5fbb922f23967f4586665d048601737ac
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-04-27 10:44:48 -0300

    Finalizando portal imobiliário

--- pages/includes/header.php
+++ pages/includes/header.php
@@ -1,75 +1,70 @@
 <?php
-    $parametros = \views\mainView::$par;
+	$parametros = \views\mainView::$par;
 ?>
-
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal Imobiliário</title>
-    <link rel="stylesheet" href="<?php echo INCLUDE_PATH ?>estilo/style.css">
-    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>estilo/css/all.css">  <!--load all styles fontawesomne-->
-    
+	<title>Portal Imobiliário</title>
+	<meta charset="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH ?>estilo/style.css">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH ?>estilo/font-awesome.min.css">
 </head>
 <body>
-<base base="<?php echo INCLUDE_PATH; ?>"/>
+<base base="<?php echo INCLUDE_PATH; ?>" />
 
 <header>
-    <div class="container">
-        <div class="logo"><a style="color:inherit;" href="<?php echo INCLUDE_PATH ?>"> Portal Imobiliário</a></div>
-        <nav class="desktop">      
-            <ul>
-            <?php
-                $selectEmpreend = \MySql::conectar()->prepare("SELECT * FROM `tb_admin.empreendimentos` ORDER BY order_id ASC");
-                $selectEmpreend->execute();
-                $empreendimentos = $selectEmpreend->fetchAll();
-                foreach ($empreendimentos as $key => $value) {                    
-            ?>
-                <li><a href=" <?php echo INCLUDE_PATH.$value['slug']; ?> "> <?php echo $value['nome']; ?></a></li>
-            <?php } ?>
-            </ul>
-        </nav>
-        <div class="clear"></div>
-    </div><!--container-->
+	<div class="container">
+	<div class="logo"><a style="color:inherit;" href="<?php echo INCLUDE_PATH; ?>">Portal Imobiliário</a></div>
+	<nav class="desktop">
+
+		<ul>
+		<?php
+			$selectEmpreend = \MySql::conectar()->prepare("SELECT * FROM `tb_admin.empreendimentos` ORDER BY order_id ASC");
+			$selectEmpreend->execute();
+			$empreendimentos = $selectEmpreend->fetchAll();
+			foreach ($empreendimentos as $key => $value) {
+		?>
+			<li><a href="<?php echo INCLUDE_PATH.$value['slug']; ?>"><?php echo $value['nome']; ?></a></li>
+		<?php } ?>
+		</ul>
+
+	</nav>
+	<div class="clear"></div>
+	</div>
 </header>
 
 <section class="search1">
-    <div class="container">
-        <h2>O que você procura?</h2>
-        <input type="text" name="texto-busca">
-    </div><!--container-->
+	<div class="container">
+		<h2>O que você procura?</h2>
+		<input type="text" name="texto-busca" />
+	</div>
 </section>
 
 <section class="search2">
-    <div class="container">
-        <form action="<?php echo INCLUDE_PATH ?>ajax/formularios.php" method="post">
-            <div class="form-group">
-                <label>Área mínima: </label>
-                <input type="number" name="areao_minima" min="0" step="10">
-            </div><!--form-group-->
-
-            <div class="form-group">
-                <label>Área máxima: </label>
-                <input type="number" name="areao_maxima" min="0" step="10">
-            </div><!--form-group-->
-
-            <div class="form-group">
-                <label>Preço mínimo: </label>
-                <input type="text" name="preco_min">
-            </div><!--form-group-->
-
-            <div class="form-group">
-                <label>Preço máximo: </label>
-                <input type="text" name="preco_max">
-            </div><!--form-group-->
-            <?php 
-                if(isset($parametros['slug_empreendimento'])){
-                    echo '<input type="hidden" name"slug_empreendimento" value"'.$parametros['slug_empreendimento'].'"">';
-                }
-            ?>
-        </form>
-        <div class="clear"></div>
-    </div><!--container-->
+	<div class="container">
+		<form method="post" action="<?php echo INCLUDE_PATH ?>ajax/formularios.php">
+			<div class="form-group">
+				<label>Área Minima: </label>
+				<input placeholder="0" name="area_minima" type="number" min="0" max="100000" step="100">
+			</div><!--form-group-->
+			<div class="form-group">
+				<label>Área Máxima: </label>
+				<input placeholder="0" name="area_maxima" type="number" min="0" max="100000" step="100">
+			</div><!--form-group-->
+			<div class="form-group">
+				<label>Preço Minimo: </label>
+				<input placeholder="0" name="preco_min" type="text">
+			</div><!--form-group-->
+			<div class="form-group">
+				<label>Preço Máximo: </label>
+				<input placeholder="0" name="preco_max" type="text">
+			</div><!--form-group-->
+			<?php
+				if(isset($parametros['slug_empreendimento'])){
+					echo '<input type="hidden" name="slug_empreendimento" value="'.$parametros['slug_empreendimento'].'" />';
+				}
+			?>
+			<div class="clear"></div>
+		</form>
+	</div>
 </section>
-    
